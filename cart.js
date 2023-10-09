8bc6300c32cc14592ccdedf4b4a89aee652b1328
Flavien Ptrk <flavien.patriarca2002@gmail.com>
2023-10-09 23:47:36 +0200

    add loal storage

--- cart.js
+++ cart.js
@@ -1,19 +1,40 @@
-
 var taxRate = 0.05;
 var shippingRate = 10.00;
 var fadeTime = 300;
 
-$('.product-quantity input').change(function () {
-    updateQuantity(this);
-});
 
-$('.product-removal button').click(function () {
-    removeItem(this);
+$(document).ready(function () {
+    // Charge les données du panier au chargement de la page
+    var cartData = JSON.parse(localStorage.getItem('cartData'));
+    if (cartData) {
+        $('#cart-subtotal').html(cartData.subtotal);
+        $('#cart-tax').html(cartData.tax);
+        $('#cart-shipping').html(cartData.shipping);
+        $('#cart-total').html(cartData.total);
+    }
+
+    var cartItems = JSON.parse(localStorage.getItem('cartItems'));
+    if (cartItems) {
+
+        for (var i = 0; i < cartItems.length; i++) {
+            var item = cartItems[i];
+            var productRow = $('.product:contains("' + item.name + '")');
+            productRow.find('.product-quantity input').val(item.quantity);
+            updateQuantity(productRow.find('.product-quantity input'));
+        }
+    }
+
+    $('.product-quantity input').change(function () {
+        updateQuantity(this);
+    });
+
+    $('.product-removal button').click(function () {
+        removeItem(this);
+    });
 });
 
 function recalculateCart() {
     var subtotal = 0;
-
 
     $('.product').each(function () {
         subtotal += parseFloat($(this).children('.product-line-price').text());
@@ -22,6 +43,15 @@ function recalculateCart() {
     var tax = subtotal * taxRate;
     var shipping = (subtotal > 0 ? shippingRate : 0);
     var total = subtotal + tax + shipping;
+
+
+    var cartData = {
+        subtotal: subtotal.toFixed(2),
+        tax: tax.toFixed(2),
+        shipping: shipping.toFixed(2),
+        total: total.toFixed(2)
+    };
+    localStorage.setItem('cartData', JSON.stringify(cartData));
 
     $('.totals-value').fadeOut(fadeTime, function () {
         $('#cart-subtotal').html(subtotal.toFixed(2));
@@ -38,12 +68,10 @@ function recalculateCart() {
 }
 
 function updateQuantity(quantityInput) {
-
     var productRow = $(quantityInput).parent().parent();
     var price = parseFloat(productRow.children('.product-price').text());
     var quantity = $(quantityInput).val();
     var linePrice = price * quantity;
-
 
     productRow.children('.product-line-price').each(function () {
         $(this).fadeOut(fadeTime, function () {
@@ -52,13 +80,44 @@ function updateQuantity(quantityInput) {
             $(this).fadeIn(fadeTime);
         });
     });
+
+
+    var cartItems = [];
+    $('.product').each(function () {
+        var productName = $(this).find('.product-title').text();
+        var productPrice = parseFloat($(this).find('.product-price').text());
+        var productQuantity = parseInt($(this).find('.product-quantity input').val());
+
+        cartItems.push({
+            name: productName,
+            price: productPrice,
+            quantity: productQuantity
+        });
+    });
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
 }
 
 function removeItem(removeButton) {
-
     var productRow = $(removeButton).parent().parent();
     productRow.slideUp(fadeTime, function () {
         productRow.remove();
         recalculateCart();
+
+
+        var cartItems = [];
+        $('.product').each(function () {
+            var productName = $(this).find('.product-title').text();
+            var productPrice = parseFloat($(this).find('.product-price').text());
+            var productQuantity = parseInt($(this).find('.product-quantity input').val());
+
+            cartItems.push({
+                name: productName,
+                price: productPrice,
+                quantity: productQuantity
+            });
+        });
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
     });
 }
+
+
