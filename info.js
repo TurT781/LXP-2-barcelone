183537cd380145e9a95015cdb08dc162fc94680f
Nolan910 <115090897+Nolan910@users.noreply.github.com>
2023-10-16 09:27:35 +0200

    db

--- info.js
+++ info.js
@@ -1,26 +1,36 @@
-const reponse = await fetch("info.json");
+fetch("http://localhost:3000/products")
+.then((response)=> response.json())
+.then((data)=> console.log(data))
+.catch((error) => console.error("Error:", error));
+
+
+const reponse = await fetch("http://localhost:3000/products");
 const book = await reponse.json();
 
 const article = book[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} €`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie;
+const imageElement = document.createElement("img");
+imageElement.src = article.URL_image;
 const synopsisElement = document.createElement("p");
 synopsisElement.innerText = article.synopsis;
-
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(synopsisElement)
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} €`;
 
 
+const productContainer = document.createElement("div");
+productContainer.appendChild(imageElement);
+productContainer.appendChild(nomElement);
+productContainer.appendChild(prixElement);
+productContainer.appendChild(categorieElement);
+productContainer.appendChild(synopsisElement)
+
+const sectionFiches = document.getElementById("fiches");
+sectionFiches.appendChild(productContainer);
 
 
-localStorage.setItem("bookName", "The book thief")
+
+
+//localStorage.setItem("bookName", "The book thief")
